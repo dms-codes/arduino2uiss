5bc18962533276bf85498d5fecd1d7839a76b87e
Donny Marthen Sitompul <donnymarthen.sitompul@gmail.com>
2024-10-19 05:45:05 +0200

    Update arduino2uiss.py

--- arduino2uiss.py
+++ arduino2uiss.py
@@ -1,23 +1,60 @@
+import serial
+import time
+
+def read_serial_data(port: str, baudrate: int = 9600) -> str:
+    """
+    Reads data from the specified serial port.
+
+    Args:
+        port (str): The serial port to read from (e.g., 'COM6').
+        baudrate (int): Baud rate for serial communication. Default is 9600.
+
+    Returns:
+        str: The decoded and stripped serial data as a string.
+    """
+    with serial.Serial(port, baudrate, timeout=1) as ser:
+        data = ser.readline().decode().rstrip()
+    return data
+
+def format_temperature(temp: str) -> str:
+    """
+    Formats the temperature string by ensuring it is at least two digits.
+
+    Args:
+        temp (str): The temperature string to format.
+
+    Returns:
+        str: The formatted temperature string.
+    """
+    return temp.zfill(3) if int(temp) < 100 else temp
+
+def save_to_file(filename: str, content: str) -> None:
+    """
+    Saves the provided content to a file.
+
+    Args:
+        filename (str): The name of the file to write to.
+        content (str): The content to be written into the file.
+    """
+    with open(filename, "w") as f:
+        f.write(content)
+
 def main():
-    import serial
-    import time
-    import math
+    port = 'COM6'
 
     while True:
-        ser = serial.Serial('COM6', 9600)
-        b = ser.readline()  # read a byte string
-        string_n = b.decode()  # decode byte string into Unicode
-        res = string_n.rstrip()
-        ser.close()
-        humi = res.split("t")[0][1:]
-        temp = res.split("t")[1]
-        if int(temp)<100:
-            temp = "0"+temp
-        print(temp)
-        result = "!0619.80S/10648.25E_000/000g000t{}h{}DMS".format(temp,humi)
-        f = open("data.txt","w")
-        f.write(result)
-        print(result)
+        data = read_serial_data(port)
+        if "t" in data:  # Ensure the data contains a temperature reading
+            humi, temp = data.split("t")
+            humi = humi[1:]  # Removing the first character from humidity
+
+            temp = format_temperature(temp)
+            result = f"!0619.80S/10648.25E_000/000g000t{temp}h{humi}DMS"
+            
+            save_to_file("data.txt", result)
+            print(result)
+        else:
+            print("Invalid data format")
 
 if __name__ == '__main__':
     main()
